e49e7ffa50165c24f3b28cafc784c2165b816d34
Ben Smith <ben@benjaminasmith.com>
2013-03-22 22:26:45 +0000

    Switch to rspec, build a few first tests for present tense conjugation.

--- test.rb
+++ test.rb
@@ -1,19 +1,47 @@
+# encoding: UTF-8
+
 require_relative 'app.rb'
-require 'test/unit'
+require 'rspec'
 require 'rack/test'
 
-class GrokilyTest < Test::Unit::TestCase
+describe "Grokily" do
   include Rack::Test::Methods
 
   def app
-    Grokily 
+    Grokily
   end
 
-  def test_root_route
+  it "redirects to Github on root" do
     get '/'
-    # Should redirect to the github repo.
+    last_response.should be_redirect
     follow_redirect!
-    assert_equal "https://github.com/benjaminasmith/grokily", last_request.url
+    last_request.url.should == "https://github.com/benjaminasmith/grokily"
+  end
+
+  # Present tense tests
+
+  it "conjugates the regular verb få in the present tense" do
+    # Should be får
+    get '/norsk/få/presens'
+    last_response.body.should == "får"
+  end
+
+  it "conjugates the irregular verb burde in the present tense" do
+    # Should be bør
+    get '/norsk/burde/presens'
+    last_response.body.should == "bør"
+  end
+
+  it "conjugates the irregular verb finnes in the present tense" do
+    # Should be finnes 
+    get '/norsk/finnes/presens'
+    last_response.body.should == "finnes"
+  end
+
+  it "fails to conjugate unknown verbs in the present tense" do
+    # Should be a 404
+    get '/norsk/finish/presens'
+    last_response.ok? == false 
   end
 
 end
