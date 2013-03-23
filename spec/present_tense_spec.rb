6c797653a522d844d8ac6af449e47c50487091ae
Ben Smith <ben@benjaminasmith.com>
2013-03-23 15:11:24 +0000

    Encode URIs so that Ruby can handle the unicode.

--- spec/present_tense_spec.rb
+++ spec/present_tense_spec.rb
@@ -5,13 +5,13 @@ describe "Grokily" do
 
   context "regular verb få" do
     it "conjugates in the present tense to får" do
-      get '/norsk/få/present'
+      get URI.encode '/norsk/få/present'
       last_response.should be_ok
       last_response.body.should == "får"
     end
 
     it "conjugates in presens to får" do
-      get '/norsk/få/presens'
+      get URI.encode '/norsk/få/present'
       last_response.should be_ok
       last_response.body.should == "får"
     end
@@ -19,13 +19,13 @@ describe "Grokily" do
 
   context "irregular verb burde" do
     it "conjugates in the present tense to bør" do
-      get '/norsk/burde/present'
+      get URI.encode '/norsk/burde/present'
       last_response.should be_ok
       last_response.body.should == "bør"
     end
 
     it "conjugates in presens to bør" do
-      get '/norsk/burde/presens'
+      get URI.encode '/norsk/burde/presens'
       last_response.should be_ok
       last_response.body.should == "bør"
     end
@@ -33,13 +33,13 @@ describe "Grokily" do
 
   context "irregular verb finnes" do
     it "conjugates in the present tense to finnes" do
-      get '/norsk/finnes/present'
+      get URI.encode '/norsk/finnes/present'
       last_response.should be_ok
       last_response.body.should == "finnes"
     end
 
     it "conjugates in presens to finnes" do
-      get '/norsk/finnes/presens'
+      get URI.encode '/norsk/finnes/presens'
       last_response.should be_ok
       last_response.body.should == "finnes"
     end
@@ -47,12 +47,12 @@ describe "Grokily" do
 
   context "unknown verb" do
     it "fails to conjugate in the present tense" do
-      get '/norsk/finish/present'
+      get URI.encode '/norsk/test/present'
       last_response.should be_not_found
     end
 
     it "fails to conjugate in presens" do
-      get '/norsk/finish/presens'
+      get URI.encode '/norsk/test/presens'
       last_response.should be_not_found
     end
   end
