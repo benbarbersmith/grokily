9383eb5af566aa98a8575345e4757b5e22b1db5d
Ben Smith <ben@benjaminasmith.com>
2013-03-24 19:20:11 +0000

    Refactor some common laguage functionality into the base class. Add support for verb lists to support new routes exposed by the application. Add new language-related exception classes to provide some granularity at the router level when catching errors.

--- lib/base/language.rb
+++ lib/base/language.rb
@@ -2,27 +2,66 @@
 require_relative 'tense'
 
 class Language
+  private
+
+  def initialize
+    @verbs = {}
+    load_verbs
+    @tenses = {}
+    load_tenses 
+  end
+
   public
 
   def register_tense(key, tense)
     @tenses[key] = tense
   end
 
+  def verbs
+    {:regular_verbs   => @verbs.values.select {|v| v if v.regular? },
+     :irregular_verbs => @verbs.values.select {|v| v unless v.regular? } }
+  end
+
+  def conjugate(verb, tense, subject=false)
+    @tenses[tense].conjugate(@verbs[verb])if has_verb? verb and has_tense? tense 
+  end
+
   private
 
-  def has_subject? subject
-    @subjects.each {|s| return subject if [s.upcase, s.downcase, s].include? subject }
-    raise LanguageException, "Unknown subject #{subject}"
+  def load_verbs 
+  end
+
+  def load_tenses
+    Dir[File.dirname(__FILE__) + "/*_tense.rb"].each {|file|
+      require file
+      tense = /#{File.dirname(__FILE__)}\/(.*)_tense.rb/.match(file)[1]
+      classname = "#{self.class}#{tense.capitalize}Tense"
+      Object.const_get(classname).register(self)
+    }
   end
 
   def has_tense? tense
-    @tenses.key? tense or raise LanguageException, "Unknown tense #{tense}"
+    @tenses.key? tense or raise TenseException, "Unknown tense #{tense}"
   end
 
   def has_verb? verb
-    @verbs.key? verb or raise LanguageException, "Unknown verb #{verb}"
+    @verbs.key? verb or raise VerbException, "Unknown verb #{verb}"
   end
+
+  def has_subject? subject
+    @subjects.map {|s| s.upcase }.include? subject.upcase  or raise SubjectException, "Unknown subject #{subject}"
+  end
+
 end
 
 class LanguageException < Exception
+end
+
+class VerbException < LanguageException
+end
+
+class TenseException < LanguageException
+end
+
+class SubjectException < LanguageException
 end
--- lib/languages/norsk/language.rb
+++ lib/languages/norsk/language.rb
@@ -2,20 +2,7 @@
 
 class Norsk < Language
   def initialize
-    @tenses = {}
-    Dir[File.dirname(__FILE__) + "/*_tense.rb"].each {|file|
-      require file
-      tense = /#{File.dirname(__FILE__)}\/(.*)_tense.rb/.match(file)[1]
-      classname = "#{self.class}#{tense.capitalize}Tense"
-      Object.const_get(classname).register(self)
-    }
     @subjects = ["Jeg", "Du", "Han", "Hun", "Vi", "De", "Den", "Det", "Dere"]
-    @verbs =  {}
-  end
-
-  def conjugate(verb, tense, subject=false)
-    if has_verb? verb and has_tense? tense 
-      @tenses[tense].conjugate(@verbs[verb])
-    end
+    super
   end
 end
