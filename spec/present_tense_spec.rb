57e7b9f6a02ce1049cd5eacf1103c0cbbe8c9e1e
Ben Smith <ben@benjaminasmith.com>
2013-03-25 23:15:16 +0000

    Add more present tense tests to catch some edge cases I've missed. Currently failing.

--- spec/present_tense_spec.rb
+++ spec/present_tense_spec.rb
@@ -31,6 +31,76 @@ describe "In the present tense, Grokily" do
     end
   end
 
+  context "conjugates the irregular verb lide to lider" do
+    it "using present" do
+      get URI.encode '/norsk/lide/present'
+      last_response.should be_ok
+      last_response.body.should == "lider"
+    end
+
+    it "using presens" do
+      get URI.encode '/norsk/lide/presens'
+      last_response.should be_ok
+      last_response.body.should == "lider"
+    end
+  end
+
+  context "conjugates the irregular verb li to lider" do
+    it "using present" do
+      get URI.encode '/norsk/li/present'
+      last_response.should be_ok
+      last_response.body.should == "lider"
+    end
+
+    it "using presens" do
+      get URI.encode '/norsk/li/presens'
+      last_response.should be_ok
+      last_response.body.should == "lider"
+    end
+  end
+
+  context "conjugates the irregular verb synes to synes or syns" do
+    it "using present" do
+      get URI.encode '/norsk/synes/present'
+      last_response.should be_ok
+      last_response.body.should == "synes / syns"
+    end
+
+    it "using presens" do
+      get URI.encode '/norsk/synes/presens'
+      last_response.should be_ok
+      last_response.body.should == "synes / syns"
+    end
+  end
+
+  context "conjugates the irregular verb flyge to synes or flyr" do
+    it "using present" do
+      get URI.encode '/norsk/flyge/present'
+      last_response.should be_ok
+      last_response.body.should == "flyr"
+    end
+
+    it "using presens" do
+      get URI.encode '/norsk/flyge/presens'
+      last_response.should be_ok
+      last_response.body.should == "flyr"
+    end
+  end
+
+  context "conjugates the irregular verb fly to synes or flyr" do
+    it "using present" do
+      get URI.encode '/norsk/fly/present'
+      last_response.should be_ok
+      last_response.body.should == "flyr"
+    end
+
+    it "using presens" do
+      get URI.encode '/norsk/fly/presens'
+      last_response.should be_ok
+      last_response.body.should == "flyr"
+    end
+  end
+
   context "conjugates the irregular verb fly to flyr" do
     it "using present" do
       get URI.encode '/norsk/fly/present'
