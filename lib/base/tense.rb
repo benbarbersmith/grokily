fb5d3c0d8ead94bdc9ef10b5568f495ba4183600
Ben Smith <ben@benjaminasmith.com>
2013-03-25 22:50:55 +0000

    Support "(seg)" verbs in the same was as transitive and intransitive, generalising where possible.

--- lib/base/tense.rb
+++ lib/base/tense.rb
@@ -1,6 +1,8 @@
 # encoding: UTF-8
 
 class Tense
+  Qualifiers = ["(transitive)", "(intransitive)", "(seg)"]
+
   private
 
   def self.to_sym
@@ -11,5 +13,18 @@ class Tense
 
   def self.register language
     @keys.each {|key| language.register_tense(key, self) }
+  end
+
+  def self.qualified? verb
+    Qualifiers.any? do |qual|
+      verb.infinitive.end_with? qual
+    end
+  end
+
+  def self.conjugate_with_qualifier verb
+    inf = verb.infinitive.split(" ")
+    newverb = verb.dup
+    newverb.infinitive = inf.first
+    "#{regular_conjugation(newverb)} #{inf.last}"
   end
 end
--- lib/languages/norsk/present_tense.rb
+++ lib/languages/norsk/present_tense.rb
@@ -4,11 +4,10 @@ class NorskPresentTense < Tense
   @keys = "present", "presens"
 
   def self.regular_conjugation verb
-    if verb.infinitive.end_with? "s"
+    if self.qualified? verb
+      self.conjugate_with_qualifier verb
+    elsif verb.infinitive.end_with? "s"
       "#{verb.infinitive}"
-    elsif verb.infinitive.end_with? "transitive)"
-      inf = verb.infinitive.split(" ")
-      "#{inf.first}r #{inf.last}"
     else
       "#{verb.infinitive}r"
     end
