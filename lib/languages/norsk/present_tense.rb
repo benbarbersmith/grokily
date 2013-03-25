d2dab8a3574388000868657e027ad74bc6a950ad
Ben Smith <ben@benjaminasmith.com>
2013-03-25 22:19:25 +0000

    Work around the "(transitive)" and "(intransitive)" labeling when conjugating in the present tense.

--- lib/languages/norsk/present_tense.rb
+++ lib/languages/norsk/present_tense.rb
@@ -6,6 +6,9 @@ class NorskPresentTense < Tense
   def self.regular_conjugation verb
     if verb.infinitive.end_with? "s"
       "#{verb.infinitive}"
+    elsif verb.infinitive.end_with? "transitive)"
+      inf = verb.infinitive.split(" ")
+      "#{inf.first}r #{inf.last}"
     else
       "#{verb.infinitive}r"
     end
