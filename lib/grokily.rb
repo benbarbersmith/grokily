4978f820b35efba3f961d2653d66bfa75d7771b3
Ben Smith <ben@benjaminasmith.com>
2013-03-26 21:03:29 +0000

    Allow verbs to have multiple conjugations, split by a / character.

--- lib/grokily.rb
+++ lib/grokily.rb
@@ -46,7 +46,8 @@ class Grokily < Sinatra::Base
   # Allow users to specify a language, verb and tense.
   get '/:language/:verb/:tense' do |language, verb, tense|
     begin 
-      router.conjugate_verb(language, verb, tense) or halt 404
+      verb = router.conjugate_verb(language, verb, tense) or halt 404
+      if verb.is_a? Array then verb.join(" / ") else verb end
     rescue LanguageException
       halt 404
     end
