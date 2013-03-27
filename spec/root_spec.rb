c6ae0d831863ee8a3d13cf657bf11659e3a88223
Ben Smith <ben@benjaminasmith.com>
2013-03-27 21:58:42 +0000

    Add tests to check if Grokily exposes a list of available tenses for a given language.

--- spec/root_spec.rb
+++ spec/root_spec.rb
@@ -24,6 +24,26 @@ describe "Grokily" do
     end
   end
 
+  context "exposes a list of Norwegian tenses on request" do
+    it "in plaintext" do
+      get '/norsk/tenses'
+      last_response.should be_ok
+      last_response.body.should =~ /present/
+      last_response.body.should =~ /presens/
+    end
+    it "in json" do
+      get '/norsk/tenses.json'
+      last_response.should be_ok
+      last_response.header['Content-Type'].should include 'application/json'
+      response = JSON.parse(last_response.body)
+      responses.has_key?("tenses").should == true
+      tenses = responses["tenses"]
+      tenses["present"].nil?.should != true
+      tenses["present"].include?("present").should == true
+      tenses["present"].include?("presens").should == true
+    end
+  end
+
   context "exposes a list of Norwegian verbs on request" do
     it "in plaintext" do
       get '/norsk/verbs'
