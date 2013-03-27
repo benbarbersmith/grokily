6534d7b6528dc97b5ab4ed1014cc96294b897cb1
Ben Smith <ben@benjaminasmith.com>
2013-03-27 08:34:43 +0000

    Add JSON output to verb conjugations.

--- lib/grokily.rb
+++ lib/grokily.rb
@@ -29,29 +29,40 @@ class Grokily < Sinatra::Base
   end
 
   # See which verbs are available in the specified language.
-  get '/:language/verbs.?:format?' do
-    verbs = router.list_verbs(params[:language])
-    if params[:format] == "json"
+  get '/:language/verbs.?:format?' do |language, format|
+    verbs = router.list_verbs(language)
+    if format.nil? 
+      "regular verbs: " + \
+      verbs[:regular_verbs].map {|v| v["infinitive"] }.join(", ") + \
+      "\n\n" + \
+      "irregular verbs: " + \
+      verbs[:irregular_verbs].map {|v| v["infinitive"] }.join(", ")
+    elsif format == "json"
       content_type 'application/json'
       verbs.to_json
-    elsif params[:format].nil? 
-      "regular verbs: " + verbs[:regular_verbs].map {|v| v["infinitive"] }.join(", ") + \
-      "\n\n" + \
-      "irregular verbs: " + verbs[:irregular_verbs].map {|v| v["infinitive"] }.join(", ")
     else
       halt 404
     end
   end
   
   # Allow users to specify a language, verb and tense.
-  get '/:language/:verb/:tense' do |language, verb, tense|
+  get '/:language/:verb/:tense.?:json?' do |language, verb, tense, format|
     begin 
-      router.conjugate_verb(language, verb, tense).flatten.join(" / ") or halt 404
+      conjugations = router.conjugate_verb(language, verb, tense) or halt 404
     rescue LanguageException
       halt 404
     end
-  end
+    if format.nil? 
+      conjugations.join(" / ")
+    elsif format == "json"
+      content_type 'application/json'
+      conjugations.map {|c| c.to_hash }.to_json
+    else
+      halt 404
+    end
 
+  end
+ 
   not_found do
     "Page not found." 
   end
